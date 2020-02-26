38e85a7157b23351dd0b794767cbcc0214f356e8
Umberto Valente <uvalente89@gmail.com>
2020-02-26 12:03:57 +0000

    feat: add changeTurn function to Game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -9,4 +9,8 @@ describe('Game', function() {
   it('should have a turn starting at 0', function() {
     expect(game.turn).toEqual(0);
   });
+
+  it('can increment turn', function() {
+    expect(game.changeTurn()).toEqual(1);
+  })
 });
