29ce8f4fcf8d44d6a3aa14a1cb23ff46d1da30e5
Umberto Valente <uvalente89@gmail.com>
2020-02-26 12:18:15 +0000

    feat: add playerOne to Game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,16 +1,31 @@
 describe('Game', function() {
   var Game = require('../lib/game');
   var game;
+  var playerOne = {
+    name: 'Player 1',
+    sign: 'X'
+  }
 
   beforeEach(function() {
-    game = new Game();
+    game = new Game(playerOne);
   });
 
-  it('should have a turn starting at 0', function() {
-    expect(game.turn).toEqual(0);
+  describe('turn', function() {
+    it('should have a turn starting at 0', function() {
+      expect(game.turn).toEqual(0);
+    });
+  
+    it('can increment turn', function() {
+      expect(game.changeTurn()).toEqual(1);
+    });
   });
 
-  it('can increment turn', function() {
-    expect(game.changeTurn()).toEqual(1);
-  })
+  describe('player', function() {
+    it('should have a player', function() {
+      expect(game.playerOne).toEqual(playerOne)
+      expect(game.playerOne.name).toEqual('Player 1')
+      expect(game.playerOne.sign).toEqual('X')
+    });
+  });
+  
 });
