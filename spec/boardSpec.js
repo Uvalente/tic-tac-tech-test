c9dd6a19205c38d95b6a85a8834cc29fc8751abb
Umberto Valente <uvalente89@gmail.com>
2020-02-27 14:37:34 +0000

    feat: add Board.isFull

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -10,4 +10,19 @@ describe('Board', function() {
     expect(board.board.length).toEqual(3)
     expect(board.board[0].length).toEqual(3)
   });
+
+  describe('isFull', function() {
+    it('return false when the board is not full', function() {
+      expect(board.isFull()).toBeFalse()
+    })
+
+    it('return true when the board is full', function() {
+      board._board = [
+        ['X', 'X', 'X'],
+        ['X', 'X', 'X'],
+        ['X', 'X', 'X']]
+      
+      expect(board.isFull()).toBeTrue()
+    })
+  });
 });
