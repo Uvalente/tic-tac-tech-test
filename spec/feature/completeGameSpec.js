afff6a81203811792d362893144f047bdcfee4ad
Umberto Valente <uvalente89@gmail.com>
2020-02-27 12:48:45 +0000

    feat: add winner selector and game over check to Game

--- spec/feature/completeGameSpec.js
+++ spec/feature/completeGameSpec.js
@@ -11,8 +11,30 @@ describe('Complete Game', function() {
 
   describe('players turns', function() {
     it('players are alternating turns', function() {
-      expect(game.insertMove(0,0)).toEqual('X')
-      expect(game.insertMove(0,1)).toEqual('O')
+      expect(game.insertMove(0, 0)).toEqual('X')
+      expect(game.insertMove(0, 1)).toEqual('O')
+    })
+  })
+
+  describe('inserting error', function() {
+    it('player can not insert in a filled spot', function() {
+      game.insertMove(0, 0)
+      expect( function() {
+        game.insertMove(0, 0)
+      }).toThrowError('Space filled')
+    })
+  })
+
+  describe('declare winner', function() {
+    it('declare a winner and stop the game', function() {
+      game.insertMove(0, 0)
+      game.insertMove(1, 0)
+      game.insertMove(0, 1)
+      game.insertMove(1, 1)
+      expect(game.insertMove(0, 2)).toEqual('Player 1 wins!')
+      expect( function() {
+        game.insertMove(1, 2)
+      }).toThrowError('The game is over')
     })
   })
 
